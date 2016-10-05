9a60ef136f2e2eb30f527bcf10b8b01cc4f3fc2b
Zeyi Fan <fanzeyi1994@gmail.com>
2016-10-04 20:19:46 -0700

    add copy & paste support

--- main.js
+++ main.js
@@ -1,27 +1,101 @@
-const {app, BrowserWindow} = require('electron')
+const {app, BrowserWindow, Menu} = require('electron');
 
-let win
+let win = null;
 
 function createWindow () {
-  win = new BrowserWindow({width: 800, height: 600})
+  win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    minWidth: 600,
+    minHeight: 450
+  });
 
-  win.loadURL(`file://${__dirname}/haloword/main.html`)
+  win.loadURL(`file://${__dirname}/haloword/main.html`);
 
   win.on('closed', () => {
     win = null
-  })
+  });
+
+  // menu
+  var menuTemplate = [
+    {
+      label: "Halo Word",
+      submenu: [
+        {
+          label: "About Halo Word",
+          selector: "orderFrontStandardAboutPanel:"
+        },
+        {
+          type: "separator"
+        },
+        {
+          label: "Quit",
+          accelerator: "Command+Q",
+          click: () => {
+            app.quit();
+          }
+        }
+      ]
+    },
+    {
+      label: "Edit",
+      submenu: [
+        {
+          label: "Undo",
+          accelerator: "CmdOrCtrl+Z",
+          selector: "undo:"
+        },
+        {
+          label: "Redo",
+          accelerator: "Shift+CmdOrCtrl+Z",
+          selector: "redo:"
+        },
+        {
+          type: "separator"
+        },
+        {
+          label: "Cut",
+          accelerator: "CmdOrCtrl+X",
+          selector: "cut:"
+        },
+        {
+          label: "Copy",
+          accelerator: "CmdOrCtrl+C",
+          selector: "copy:"
+        },
+        {
+          label: "Paste",
+          accelerator: "CmdOrCtrl+V",
+          selector: "paste:"
+        },
+        {
+          label: "Select All",
+          accelerator: "CmdOrCtrl+A",
+          selector: "selectAll:"
+        }
+      ]
+    }
+  ];
+
+  if (process.platform !== 'darwin') {
+    // need investigate other platform
+    // TODO: window, help, services menu on macOS
+    menuTemplate.shift()
+  }
+
+  Menu.setApplicationMenu(Menu.buildFromTemplate(menuTemplate));
 }
 
-app.on('ready', createWindow)
+app.on('ready', createWindow);
 
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
-    app.quit()
+    app.quit();
   }
-})
+});
 
 app.on('activate', () => {
   if (win === null) {
-    createWindow()
+    createWindow();
   }
-})
+});
